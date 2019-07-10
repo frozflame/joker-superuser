c400f597753249b962a0a38fb523d26bf44687bc
Hailong <lendoli@163.com>
2019-07-10 18:30:15 +0800

    sus fontface

--- joker/superuser/main.py
+++ joker/superuser/main.py
@@ -9,6 +9,7 @@ from volkanic.system import CommandRegistry
 entries = {
     'joker.superuser.mkconf_apt': 'apt',
     'joker.superuser.misc.drawbox': 'box',
+    'joker.superuser.misc.fontface': 'fontface',
     'joker.superuser.python.project': 'pkg',
     'joker.superuser.shell.associative': 'shell-assoc',
     'joker.superuser.shell.quote': 'shell-quote',
