ec8d4a83d64625714174ee23468118a78fd7d0d2
Hailong <lendoli@163.com>
2019-08-01 23:52:35 +0800

    acd ~/Library/Application Support

--- resources/source/acd-abbrmap.sh
+++ resources/source/acd-abbrmap.sh
@@ -1,15 +1,16 @@
 unalias acd 2>/dev/null
 function acd() {
+    local IFS=$' \t\n'
     if [[ $1 == . ]]; then
         echo '# .' | netcat 127.0.0.1 8331
         return
-    elif [[ -d $1 ]]; then
+    elif [[ -d "$*" ]]; then
         # shellcheck disable=SC2164
-        cd "$1"
+        cd "$*"
         return
-    elif [[ -f $1 ]]; then
+    elif [[ -f "$*" ]]; then
         # shellcheck disable=SC2164
-        cd "$(dirname "$1")"
+        cd "$(dirname "$*")"
         return
     fi
 
@@ -30,6 +31,5 @@ function acd() {
         return
         ;;
     esac
-
-    cd "$(echo "$1" | sed 's:\s:_:' | netcat 127.0.0.1 8331)" || return
+    cd "$(echo "$*" | sed 's:\s:_:' | netcat 127.0.0.1 8331)" || return
 }
