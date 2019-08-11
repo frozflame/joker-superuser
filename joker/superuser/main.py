c2accd4d7bc4674a5d89aa366f847753102ed381
Hailong <lendoli@163.com>
2019-08-11 23:37:52 +0800

    rm macos/share.py

--- joker/superuser/main.py
+++ joker/superuser/main.py
@@ -3,32 +3,14 @@
 
 from __future__ import unicode_literals
 
-import platform
 from volkanic.system import CommandRegistry
 
 entries = {
     'joker.superuser.python.directory': 'pydir',
     'joker.superuser.python.entrypoint': 'pyent',
-    'joker.superuser.unix.cases': 'cases',
-    'joker.superuser.unix.unsource': 'unsource',
+    'joker.superuser.shell.unsource': 'unsource',
+    'joker.superuser.shell.cases': 'cases',
 }
-
-_macos_entries = {
-    'joker.superuser.macos.share': 'share',
-}
-
-
-osname = platform.system().lower()
-
-if osname == 'darwin':
-    entries.update(_macos_entries)
-
-# elif osname == 'windows':
-#     handler = _windows_open
-# else:
-#     handler = _linux_open
-# for path in paths:
-#     handler(path)
 
 registry = CommandRegistry(entries)
 
