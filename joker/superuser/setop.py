bda652324d8a2d219863aabe3e3033a70de64406
Hailong <lendoli@163.com>
2020-03-18 12:16:54 +0800

    ShellStream() -> ShellStream.open()

--- joker/superuser/setop.py
+++ joker/superuser/setop.py
@@ -5,8 +5,8 @@ from joker.stream.shell import ShellStream
 
 
 def _minus(path1, path2):
-    s1 = set(ShellStream(path1).dense())
-    s2 = set(ShellStream(path2).dense())
+    s1 = set(ShellStream.open(path1).dense())
+    s2 = set(ShellStream.open(path2).dense())
     for x in s1 - s2:
         print(x)
 
