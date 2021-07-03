a8092a2015063c6f8f1f26197654b439b4cccdd3
frozflame <lendoli@163.com>
2021-07-03 15:25:27 +0800

    move functional modules to tools/

--- joker/superuser/__main__.py
+++ joker/superuser/__main__.py
@@ -8,11 +8,11 @@ import volkanic
 cmddef = """
 pydir       joker.superuser.tools.pydir
 pyentry     joker.superuser.tools.pyentry
-unsource    joker.superuser.unsource
-cases       joker.superuser.cases
-dup         joker.superuser.dedup
-setop       joker.superuser.setop
-rmdir       joker.superuser.remove
+unsource    joker.superuser.tools.unsource
+cases       joker.superuser.tools.cases
+dup         joker.superuser.tools.dedup
+setop       joker.superuser.tools.setop
+rmdir       joker.superuser.tools.remove
 apt         joker.superuser.tools.apt
 url         joker.superuser.tools.urls
 urls        joker.superuser.tools.urls:runloop
