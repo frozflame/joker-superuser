e802057b6d4b6561f8371d40c7d1e9f4b3352005
frozflame <frozflame@outlook.com>
2021-09-18 21:22:36 +0800

    pydir.py: nsp pakcage styles, default native; rename dot_name, hyf_name, uns_name

--- joker/superuser/tools/pydir.py
+++ joker/superuser/tools/pydir.py
@@ -15,10 +15,9 @@ from joker.superuser.environ import GlobalInterface
 _gi = GlobalInterface()
 _logger = logging.Logger(__name__)
 
-
 _nspinits = {
-    'i': '__import__("pkg_resources").declare_namespace(__name__)',
-    'p': '__path__ = __import__("pkgutil").extend_path(__path__, __name__)',
+    'pkg_resources': '__import__("pkg_resources").declare_namespace(__name__)',
+    'pkgutil': '__path__ = __import__("pkgutil").extend_path(__path__, __name__)',
 }
 
 
@@ -35,33 +34,29 @@ class ProjectDirectoryMaker(object):
         self.nsp = nsp.strip()
         self.pkg = pkg.strip()
         if self.nsp:
-            names = [self.nsp, self.pkg]
+            self.names = [self.nsp, self.pkg]
         else:
-            names = [self.pkg]
-        self.names = names
-        self.dot_name = '.'.join(names)
-        self.hyf_name = '-'.join(names)
-        self.uns_name = '_'.join(names)
+            self.names = [self.pkg]
 
-        self.package_name = '.'.join(names)
-        self.project_name = '-'.join(names)
-        self.identifier = '_'.join(names)
+        # self.dot_name = '.'.join(names)
+        # self.hyf_name = '-'.join(names)
+        # self.uns_name = '_'.join(names)
 
+        self.package_name = '.'.join(self.names)
+        self.project_name = '-'.join(self.names)
+        self.identifier = '_'.join(self.names)
         self.common_dirs = {
-            'docs': self.under_proj_dir('docs'),
-            'test': self.under_proj_dir('test_' + self.uns_name),
-            'templates': self.under_pkg_dir('templates'),
+            'docs': self.under_project_dir('docs'),
+            'test': self.under_project_dir('tests'),
+            'templates': self.under_package_dir('templates'),
         }
         self.common_files = _vk_join({
-            '__init__.py': self.under_pkg_dir(),
-            'requirements.txt': self.hyf_name,
-            'MANIFEST.in': self.hyf_name,
-            '.gitignore': self.hyf_name,
-            'setup.py': self.hyf_name,
+            '__init__.py': self.under_package_dir(),
+            'requirements.txt': self.project_name,
+            'MANIFEST.in': self.project_name,
+            '.gitignore': self.project_name,
+            'setup.py': self.project_name,
         })
-        if self.nsp:
-            p = join(self.hyf_name, self.nsp, '__init__.py')
-            self.common_files['nspinit'] = p
 
     @classmethod
     def parse(cls, name):
@@ -74,11 +69,11 @@ class ProjectDirectoryMaker(object):
             raise ValueError('invalid pakcage name: ' + repr(name))
         return cls(mat.group(1)[:-1] or '', mat.group(2))
 
-    def under_proj_dir(self, *paths):
-        return join(self.hyf_name, *paths)
+    def under_project_dir(self, *paths):
+        return join(self.project_name, *paths)
 
-    def under_pkg_dir(self, *paths):
-        parts = [self.hyf_name] + self.names + list(paths)
+    def under_package_dir(self, *paths):
+        parts = [self.project_name] + self.names + list(paths)
         return join(*parts)
 
     def locate(self, name):
@@ -93,9 +88,9 @@ class ProjectDirectoryMaker(object):
             open(path, 'a').close()
 
     def sub(self, text):
-        text = text.replace('__sus_hyf_name', self.hyf_name)
-        text = text.replace('__sus_dot_name', self.dot_name)
-        text = text.replace('__sus_uns_name', self.uns_name)
+        text = text.replace('__sus_hyf_name', self.project_name)
+        text = text.replace('__sus_dot_name', self.package_name)
+        text = text.replace('__sus_uns_name', self.identifier)
         text = text.replace('__sus_nsp', self.nsp)
         text = text.replace('__sus_pkg', self.pkg)
         return text
@@ -116,10 +111,10 @@ class ProjectDirectoryMaker(object):
         return os.linesep.join([
             'include requirements.txt',
             'exclude {}/*'.format(
-                relpath(self.common_dirs['test'], self.hyf_name),
+                relpath(self.common_dirs['test'], self.project_name),
             ),
             'recursive-include {} *.html'.format(
-                relpath(self.common_dirs['templates'], self.hyf_name),
+                relpath(self.common_dirs['templates'], self.project_name),
             )
         ]) + os.linesep
 
@@ -129,8 +124,13 @@ class ProjectDirectoryMaker(object):
             with open(path, 'a') as fout:
                 fout.write(content + os.linesep)
 
-    def write_nspinit(self, approach):
-        self.write('nspinit', _nspinits.get(approach))
+    def write_nspinit(self, style):
+        content = _nspinits.get(style)
+        if not content:
+            return
+        path = self.under_project_dir(self.nsp, '__init__.py')
+        with open(path, 'a') as fout:
+            fout.write(content)
 
     def write_version_variable(self):
         self.write('__init__.py', "__version__ = '0.0.1'")
@@ -149,9 +149,9 @@ class ProjectDirectoryMaker(object):
         self.write('.gitignore', open(path).read())
 
 
-def make_project(name, alt, setup, gitignore, require, nsp_approach):
+def make_project(name, alt, setup, gitignore, require, nsp_style):
     if alt == 'nspinit':
-        print(_nspinits.get(nsp_approach, ''))
+        print(_nspinits.get(nsp_style, ''))
         return
     if alt == 'gitignore':
         print(open(under_asset_dir('gitignore.txt')).read())
@@ -182,7 +182,7 @@ def make_project(name, alt, setup, gitignore, require, nsp_approach):
     mkr.write_gitignore(gitignore)
     mkr.write_requirements(require or [])
     mkr.write_manifest()
-    mkr.write_nspinit(nsp_approach)
+    mkr.write_nspinit(nsp_style)
     mkr.write_version_variable()
 
 
@@ -191,7 +191,7 @@ def run(prog=None, args=None):
     desc = 'Generate a python project structure'
     # s = ' (case sensitive)'
     epilog = '\n'.join([
-        format_help_section('Namespace package approaches', _nspinits),
+        format_help_section('Namespace package styles', _nspinits),
         'About namespace packages:',
         '  https://packaging.python.org/guides/packaging-namespace-packages/',
     ])
@@ -205,8 +205,9 @@ def run(prog=None, args=None):
         choices=['nspinit', 'gitignore', 'sub', 'setup', 'manifest'],
         help='alternative action')
 
-    add('-n', '--nsp-approach', choices=['i', 'p', 'e'],
-        default='i', help='namespace package approach')
+    # https://packaging.python.org/guides/packaging-namespace-packages/
+    add('-n', '--nsp-style', choices=list(_nspinits),
+        default='native', help='namespace package style')
 
     add('-s', '--setup', metavar='template_setup.py',
         help='path to a custom setup.py template')
