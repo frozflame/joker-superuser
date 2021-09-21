c08d3feb11801b40518eefcd5e3fc0ebca1d83e0
frozflame <frozflame@outlook.com>
2021-09-21 13:07:11 +0800

    change joker.default => joker.environ.provincial

--- joker/superuser/utils.py
+++ joker/superuser/utils.py
@@ -7,17 +7,17 @@ from functools import wraps
 
 def under_asset_dir(*paths):
     import joker.superuser
-    from joker.default import under_package_dir
+    from volkanic.utils import under_package_dir
     return under_package_dir(joker.superuser, 'asset', *paths)
 
 
 def under_joker_superuser_dir(*paths):
-    from joker.default import under_joker_dir
+    from joker.environ.provincial import under_joker_dir
     return under_joker_dir('superuser', *paths)
 
 
 def make_joker_superuser_dir(*paths):
-    from joker.default import make_joker_dir
+    from joker.environ.provincial import make_joker_dir
     return make_joker_dir('superuser', *paths)
 
 
