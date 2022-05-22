7660d366c307c49ab1e3244a3f321b12f809129a
frozflame <frozflame@outlook.com>
2022-05-22 13:29:57 +0800

    add chksums.py -- rename subcmd chksumdirs to chksum-rename, new subcmds sha1b32, sha384b64

--- joker/superuser/__main__.py
+++ joker/superuser/__main__.py
@@ -4,18 +4,21 @@
 import volkanic
 
 cmddef = """
-l           joker.superuser.cli.locators:main
-chksumdirs  joker.superuser.cli.checksumdirs:main
-pydir       joker.superuser.tools.pydir
-pyentry     joker.superuser.tools.pyentry
-unsource    joker.superuser.tools.unsource
-cases       joker.superuser.tools.cases
-dup         joker.superuser.tools.dedup
-setop       joker.superuser.tools.setop
-rmdir       joker.superuser.tools.remove
-apt         joker.superuser.tools.apt
-url         joker.superuser.tools.urls
-urls        joker.superuser.tools.urls:runloop
+l               joker.superuser.cli.locators:main
+sha1b32         joker.superuser.cli.chksums:sha1b32
+sha384b64       joker.superuser.cli.chksums:sha384b64
+chksum-rename   joker.superuser.cli.chksums:main
+uuid            joker.superuser.cli.uuidgen:main
+pydir           joker.superuser.tools.pydir
+pyentry         joker.superuser.tools.pyentry
+unsource        joker.superuser.tools.unsource
+cases           joker.superuser.tools.cases
+dup             joker.superuser.tools.dedup
+setop           joker.superuser.tools.setop
+rmdir           joker.superuser.tools.remove
+apt             joker.superuser.tools.apt
+url             joker.superuser.tools.urls
+urls            joker.superuser.tools.urls:runloop
 """
 
 _prog = 'python3 -m joker.superuser'
